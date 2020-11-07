f30371ec503f24740797d2d40a9d32db0f235c8b
liuhu <1176304452@qq.com>
2020-11-07 14:23:59 +0800

    feat: reducer_hook

--- hook/src/App.js
+++ hook/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import useReducer from './hook/reducer';
 
 
 // function FriendStatus(props) {
@@ -16,21 +17,21 @@ import React, { useState, useEffect } from 'react';
 //     }
 //     return isOnline ? 'Online' : 'Offline'
 // }
-function App() {
-    const [ count, setCount ] = useState(0);
-    useEffect(() => {
-        document.write = `you click ${count} times` 
+// function App() {
+//     const [ count, setCount ] = useState(0);
+//     useEffect(() => {
+//         document.write = `you click ${count} times` 
         
-    })
-    return (
-        <div>
-            <p>you click {count} times</p>
-            {/* <button onClick={() => setCount(count + 1)}>click</button> */}
-            <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
-            <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
-        </div>
-    )
-}
+//     })
+//     return (
+//         <div>
+//             <p>you click {count} times</p>
+//             {/* <button onClick={() => setCount(count + 1)}>click</button> */}
+//             <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+//             <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+//         </div>
+//     )
+// }
 
 // export default App;
 // class App extends Component {
@@ -66,5 +67,29 @@ function App() {
 //         <ThemeContext.Provider value={thems.dark}></ThemeContext.Provider>
 //     )
 // }
+
+function App() {
+    const initialState = {count : 0};
+    const [ state, dispatch] = useReducer(reducer, initialState);
+
+    function reducer (state, action) {
+        switch(action.type) {
+            case 'increment': 
+                return {count: state.count + 1};
+            case 'devrement': 
+                return {count: state.count - 1};
+            default: 
+                throw new Error();
+        }
+    }
+    
+    return (
+        <div>
+            Count: {state.count}
+            <button onClick={() => dispatch({type: 'devrement'})}>-</button>
+            <button onClick={() => dispatch({type: 'increment'})}>+</button>
+        </div>
+    )
+}
 
 export default App
--- hook/src/hook/reducer.js
+++ hook/src/hook/reducer.js
@@ -1,4 +1,4 @@
-import { useState , useEffect } from 'react';
+import { useState  } from 'react';
 // function useFriendStatus (friendId) {
 //     const [ isOnline, setIsOnline ] = useState(null);
 
@@ -15,14 +15,14 @@ import { useState , useEffect } from 'react';
 //     return isOnline;
 // }
 //reducer的hook
-// function useReducer(reducer, initialState){
-//     //初始化state的值
-//     const [ state, setState ] = useState(initialState);
+function useReducer(reducer, initialState){
+    //初始化state的值
+    const [ state, setState ] = useState(initialState);
 
-//         function dispatch(action) {
-//             const nextState = reducer(state, action);
-//             setState(nextState);
-//         }
-//     return [ state,dispatch ];
-// }
-// export default useReducer;
+        function dispatch(action) {
+            const nextState = reducer(state, action);
+            setState(nextState);
+        }
+    return [ state,dispatch ];
+}
+export default useReducer;
